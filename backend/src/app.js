c4cb5f9de6b8731334e283c89b65c352f469071f
morningstar004 <pranjalkumar000007@gmail.com>
2026-04-27 21:58:50 +0530

    setting up routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,14 @@
 import express from 'express';
-import { DBname } from './config/constants.js';
+import userRouter from './routes/user_route.js';
 
 const app = express();
+// To parse incoming JSON data in the request body
+app.use(express.json());
+
+app.use('/api/v1/users', userRouter);
+// app.use('/api/v1/posts', postRouter);
+
+//expamle route : http://localhost:4050/api/v1/users/register
+
 
 export default app;
