b6f669d56faafc9311119868deb9504ce4540c72
morningstar004 <pranjalkumar000007@gmail.com>
2026-04-30 23:39:56 +0530

    added Postview and updatePost

--- backend/src/controllers/post_controller.js
+++ backend/src/controllers/post_controller.js
@@ -16,4 +16,33 @@ const createPost = async (req, res) => {
   }
 };
 
-export { createPost };
+//read post 
+
+const getPosts = async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+}
+
+//update post
+const updatePost = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, description, age } = req.body;
+
+    if(!name || !description || !age) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    const post = await Post.findByIdAndUpdate(id, { name, description, age }, { new: true });
+    if (!post) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+    res.status(200).json({ message: "Post updated successfully", post });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+}
+export { createPost, getPosts, updatePost };
--- backend/src/routes/post_route.js
+++ backend/src/routes/post_route.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { createPost } from "../controllers/post_controller.js";
+import { createPost, getPosts, updatePost } from "../controllers/post_controller.js";
 
 const router = Router();
 
 router.route('/create').post(createPost);
+router.route('/').get(getPosts);
+router.route('/update/:id').put(updatePost);
 
 export default router;
